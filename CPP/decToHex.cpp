f6f61dc6052d76b78ce95da38d917ea564a85fef
Cobi Hopkins <cobihopkins@yahoo.com>
2019-01-28 09:51:47 +0000

    Update decToHex.cpp

--- CPP/decToHex.cpp
+++ CPP/decToHex.cpp
@@ -1,1 +1,19 @@
 //Incomplete
+#include <iostream>
+using namespace std;
+
+string decToHex(int decimal) ;
+
+int main()
+{
+  
+  return 0 ;
+}
+
+string decToHex(int decimal) 
+{
+ 
+  string hex = '' ;
+  
+  
+}
