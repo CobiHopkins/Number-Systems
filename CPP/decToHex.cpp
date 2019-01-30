b8fccf12ed37f26b4abd401e7d22fdde9f598cc6
Cobi Hopkins <cobihopkins@yahoo.com>
2019-01-30 11:21:28 +0000

    Update decToHex.cpp

--- CPP/decToHex.cpp
+++ CPP/decToHex.cpp
@@ -1,19 +1,30 @@
-//Incomplete
 #include <iostream>
 using namespace std;
 
-string decToHex(int decimal) ;
+string decToHex(int decimal);
 
-int main()
+int main() 
 {
-  
-  return 0 ;
+    cout << decToHex(3017) << endl;
 }
 
-string decToHex(int decimal) 
+string decToHex(int decimal)
 {
- 
-  string hex = '' ;
-  
-  
+    string hexValue;
+    string extraValues[7] = {"A", "B", "C", "D", "E", "F", "\0"};
+    
+    while (decimal > 0)
+    {
+        if ( (decimal % 16) > 9 )
+        {
+            hexValue = extraValues[(decimal % 16) - 10] + hexValue;
+        } 
+        else 
+        {
+            hexValue = to_string(decimal % 16) + hexValue;
+        }
+        decimal = decimal / 16;
+    }
+    
+    return hexValue;
 }
